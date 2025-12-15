6a148d31848d50b56ea938660d5824757c3a0d99
ngocdaobao <dbaongoc2312@gmail.com>
2025-12-16 00:51:25 +0700

    decrease num_class

--- run.sh
+++ run.sh
@@ -49,7 +49,7 @@ python train_dreambooth_sdxl_lora.py \
   --output_dir="${class_token}_dreambooth_model" \
   --instance_prompt="a ${unique_token} ${class_token}" \
   --class_prompt="a ${class_token}" \
-  --num_class_images=100 \
+  --num_class_images=50 \
   --class_token="${class_token}" \
   --unique_token="${unique_token}" \
   --resolution=1024 \
