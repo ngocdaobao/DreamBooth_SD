cb9ec022617d0c3ad563967523cbf9ec0f7d1cc0
ngocdaobao <dbaongoc2312@gmail.com>
2025-12-16 00:31:43 +0700

    simplify prompt

--- inference.py
+++ inference.py
@@ -13,5 +13,5 @@ pipe.to("cuda")
 
 # Load LoRA
 pipe.load_lora_weights(lora_path)
-image = pipe('a sks dog in strawberry farm', num_inference_steps=50).images[0]
+image = pipe('a sks dog ', num_inference_steps=50).images[0]
 image.save("inference_output.png")
