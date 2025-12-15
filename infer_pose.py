efcbef4e8596a67e27fd2912f33da498d87118e3
ngocdaobao <dbaongoc2312@gmail.com>
2025-12-15 23:01:44 +0700

    trial for pose condition

---
+++ infer_pose.py
@@ -0,0 +1,39 @@
+import gdown
+from controlnet_aux import OpenposeDetector
+from diffusers import StableDiffusionControlNetPipeline, ControlNetModel
+import torch
+
+gdown.download_folder(
+    "https://drive.google.com/drive/folders/1h50l1oRCSXTppUmm2oZXCOwvmT7BBAIV?usp=drive_link",
+    output = 'ckpt_for_infer_pose',
+    quiet=False
+)
+
+lora_weight_path = "ckpt_for_infer_pose/pytorch_lora_weights.safetensors"
+image_for_pose = "home-page-slider-3.jpg"
+
+pose_detector = OpenposeDetector.from_pretrained("lllyasviel/ControlNet")
+controlnet = ControlNetModel.from_pretrained(
+    "lllyasviel/sd-controlnet-openpose", torch_dtype=torch.float16
+)
+
+pose_condition = pose_detector(image_for_pose)
+
+pipe = StableDiffusionControlNetPipeline.from_pretrained(
+    "stabilityai/stable-diffusion-xl-base-1.0",
+    controlnet=controlnet,
+    torch_dtype=torch.float16,
+)
+
+pipe.load_lora_weights(lora_weight_path)
+
+pipe.to("cuda")
+
+image = pipe(
+    "a sks dog in the beach",
+    pose_condition,
+    num_inference_steps=40
+).images[0]
+
+image.save("infer_pose_output.png")
+
