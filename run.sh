40abec86718d7fe877eeafc84bc963bcc3ecbf40
Bao Ngoc Dao <162009876+ngocdaobao@users.noreply.github.com>
2025-12-17 14:47:21 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -53,8 +53,8 @@ python train_dreambooth_sdxl_lora.py \
   --class_token="${class_token}" \
   --unique_token="${unique_token}" \
   --resolution=1024 \
-  --train_batch_size=2 \
-  --max_train_steps=2000 \
+  --train_batch_size=1 \
+  --max_train_steps=1000 \
   --learning_rate=5e-5 \
   --mixed_precision="fp16" \
   --prior_loss_weight=0.2 \
