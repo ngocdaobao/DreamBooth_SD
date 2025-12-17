411c3c8c7e0a2f57033cca7ca521f423e6341970
ngocdaobao <dbaongoc2312@gmail.com>
2025-12-18 01:25:28 +0700

    change pose prompt

--- inference.py
+++ inference.py
@@ -16,5 +16,6 @@ pipe.to("cuda")
 # Load LoRA
 torch.manual_seed(2)
 pipe.load_lora_weights(lora_ckpt, weight_name="pytorch_lora_weights.safetensors")
-image = pipe('a photo of sks girl in Paris street, clear face, with the head slightly tilted forward; the left arm extended forward and outward with a bent elbow, the right arm bent at the elbow and positioned closer to the torso; the right leg bent at the knee and positioned forward, and the left leg bent at the knee and positioned backward.', num_inference_steps=40).images[0]
+image = pipe('a photo of sks girl in Paris street, the head tilted forward about 10°, one arm extended forward at roughly 40° while the other is bent backward around 30°, one leg angled forward about 30° with a slight knee bend, and the other leg angled backward about 35° with a deeper knee bend.', 
+             num_inference_steps=40).images[0]
 image.save("pose_prompt_output.png")
