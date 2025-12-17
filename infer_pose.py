23f56759912680ca9af00ce7c7d53923e91ec7e9
ngocdaobao <dbaongoc2312@gmail.com>
2025-12-17 23:41:25 +0700

    add negative prompt

--- infer_pose.py
+++ infer_pose.py
@@ -9,7 +9,7 @@ link = "https://drive.google.com/file/d/1gNjI7LlcSdlJwa50Dw9p9o8wgJkfcrOH/view?u
 
 lora_ckpt = gdown.download(link, quiet=False, fuzzy=True)
 pose_detector = OpenposeDetector.from_pretrained("lllyasviel/ControlNet")
-controlnet = ControlNetModel.from_pretrained('thibault/controlnet-openpose-sdxl', torch_dtype=torch.float16)
+controlnet = ControlNetModel.from_pretrained("thibaud/controlnet-openpose-sdxl-1.0", torch_dtype=torch.float16)
 pipe = StableDiffusionXLControlNetPipeline.from_pretrained(
     "stabilityai/stable-diffusion-xl-base-1.0",
     controlnet=controlnet,
@@ -25,6 +25,7 @@ negative_prompt = 'distorted face, blurred, low quality, ugly'
 
 result = pipe(
     prompt=prompt,
+    negative_prompt=negative_prompt,
     image=pose_image,
     num_inference_steps=30,
     guidance_scale=7.5,
