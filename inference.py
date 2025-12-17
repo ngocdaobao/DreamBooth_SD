1b7ae733014dfdee79b0246078c8659c3e3c9b1b
ngocdaobao <dbaongoc2312@gmail.com>
2025-12-18 01:28:11 +0700

    remove pose prompt

--- inference.py
+++ inference.py
@@ -16,6 +16,6 @@ pipe.to("cuda")
 # Load LoRA
 torch.manual_seed(2)
 pipe.load_lora_weights(lora_ckpt, weight_name="pytorch_lora_weights.safetensors")
-image = pipe('a photo of sks girl in Paris street, the head tilted forward about 10°, one arm extended forward at roughly 40° while the other is bent backward around 30°, one leg angled forward about 30° with a slight knee bend, and the other leg angled backward about 35° with a deeper knee bend.', 
+image = pipe('a photo of sks girl in Paris street', #the head tilted forward about 10°, one arm extended forward at roughly 40° while the other is bent backward around 30°, one leg angled forward about 30° with a slight knee bend, and the other leg angled backward about 35° with a deeper knee bend.', 
              num_inference_steps=40).images[0]
 image.save("pose_prompt_output.png")
