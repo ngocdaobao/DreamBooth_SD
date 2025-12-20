382bd1b5655ddc27489d07a870cd17e20dc4bd43
ngocdaobao <dbaongoc2312@gmail.com>
2025-12-21 02:40:53 +0700

    bottom

--- infer_drive.py
+++ infer_drive.py
@@ -13,5 +13,5 @@ pipe.load_lora_weights("cat_dreambooth_model/pytorch_lora_weights.safetensors")
 pipe.to('cuda')
 
 torch.manual_seed(23)
-image = pipe(prompt ='rwt cat seen from the top', num_inference_steps=40, guidance_scale=7.5).images[0]
-image.save('cat_top.png')
+image = pipe(prompt ='rwt cat seen from the bottom', num_inference_steps=40, guidance_scale=7.5).images[0]
+image.save('cat_bottom.png')
