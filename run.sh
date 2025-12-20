e08c48f35cbb4446c4b0fa16a4a43ad35853ca56
ngocdaobao <dbaongoc2312@gmail.com>
2025-12-21 00:57:38 +0700

    change token

--- run.sh
+++ run.sh
@@ -13,7 +13,7 @@ set -euo pipefail
 #   ./run.sh            # uses defaults
 
 # Defaults
-DEFAULT_UNIQUE="sks"
+DEFAULT_UNIQUE="rwt"
 
 unique_token="$DEFAULT_UNIQUE"
 
@@ -47,8 +47,8 @@ python train_dreambooth_sdxl_lora.py \
   --instance_data_dir="dataset/${class_token}" \
   --class_data_dir="${class_token}_class_images" \
   --output_dir="${class_token}_dreambooth_model" \
-  --instance_prompt="a ${unique_token} ${class_token}" \
-  --class_prompt="a ${class_token}" \
+  --instance_prompt="a ${unique_token} dog" \
+  --class_prompt="a dog" \
   --num_class_images=200 \
   --class_token="${class_token}" \
   --unique_token="${unique_token}" \
