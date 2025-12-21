e6ec479a44246503a570c8643cc0d9a956ad5552
ngocdaobao <dbaongoc2312@gmail.com>
2025-12-21 17:14:17 +0700

    change prompt

--- infer_drive.py
+++ infer_drive.py
@@ -12,6 +12,27 @@ pipe = StableDiffusionXLPipeline.from_pretrained(
 pipe.load_lora_weights("dog2_dreambooth_model/pytorch_lora_weights.safetensors")
 pipe.to('cuda')
 
-torch.manual_seed(100)
+torch.manual_seed(240)
+image = pipe(prompt ='rwt dog wearing superman outfit', num_inference_steps=40, guidance_scale=7.5).images[0]
+image.save('dog_superman.png')
+
+image = pipe(prompt ='rwt dog wearing wizard outfit', num_inference_steps=40, guidance_scale=7.5).images[0]
+image.save('dog_wizard.png')
+
+image = pipe(prompt ='rwt dog wearing police outfit', num_inference_steps=40, guidance_scale=7.5).images[0]
+image.save('dog_police.png')
+
+image = pipe(prompt ='rwt dog wearing nurse outfit', num_inference_steps=40, guidance_scale=7.5).images[0]
+image.save('dog_nurse.png')
+
+image = pipe(prompt ='rwt dog wearing ironman outfit', num_inference_steps=40, guidance_scale=7.5).images[0]
+image.save('dog_ironman.png')
+
 image = pipe(prompt ='rwt dog wearing chef outfit', num_inference_steps=40, guidance_scale=7.5).images[0]
-image.save('dog.png')
+image.save('dog_chef.png')
+
+image = pipe(prompt ='rwt dog wearing angel wings', num_inference_steps=40, guidance_scale=7.5).images[0]
+image.save('dog_angel.png')
+
+image = pipe(prompt ='rwt dog wearing purple wizard outfit', num_inference_steps=40, guidance_scale=7.5).images[0]
+image.save('dog_purple_wizard.png')
