d0c832965770e4feac99a4415aafbf689000e07c
ngocdaobao <dbaongoc2312@gmail.com>
2025-12-21 15:33:56 +0700

    change seed

--- infer_drive.py
+++ infer_drive.py
@@ -12,7 +12,7 @@ pipe = StableDiffusionXLPipeline.from_pretrained(
 pipe.load_lora_weights("cat_dreambooth_model/pytorch_lora_weights.safetensors")
 pipe.to('cuda')
 
-torch.manual_seed(700)
-negative_prompt = "front view"
+torch.manual_seed(7)
+negative_prompt = "looking from the front view"
 image = pipe(prompt ='rwt cat seen from the back', negative_prompt=negative_prompt, num_inference_steps=40, guidance_scale=7.5).images[0]
 image.save('cat_back.png')
