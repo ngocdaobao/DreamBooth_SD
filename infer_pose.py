27651790c48a47eebee0b510ba66ea3670f4bb90
ngocdaobao <dbaongoc2312@gmail.com>
2026-01-02 22:25:37 +0700

    turn off controlnet

--- infer_pose.py
+++ infer_pose.py
@@ -53,9 +53,9 @@ result = pipe(
     num_inference_steps=40,
     guidance_scale=7.5,
 
-    controlnet_conditioning_scale=1.0,
-    control_guidance_start=0.0,   # 🔑 start pose late
-    control_guidance_end=1.0,
+    # controlnet_conditioning_scale=1.0,
+    # control_guidance_start=0.0,   # 🔑 start pose late
+    # control_guidance_end=1.0,
 ).images[0]
 
 result.save("no_pose.png")
